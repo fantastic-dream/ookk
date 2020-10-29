781fbe47c3250daf15653ff323f125e2f0f4cc6b
ZWkang <kang95630@gmail.com>
2020-10-29 22:02:12 +0800

    feat: initial commit

---
+++ bin/index.js
@@ -0,0 +1,36 @@
+#!/usr/bin/env node
+'use strict';
+const meow = require('meow');
+const chalk = require('chalk');
+const updateNotifier = require('update-notifier');
+const ookk = require('..');
+
+const cli = meow(`
+  Usage
+    $ ookk <url>
+
+  Options
+    --key        Google API Key.
+    --strategy   Strategy to use when analyzing the page: mobile|desktop
+
+  Example
+    $ ookk https://zwkang.com --strategy=mobile
+`);
+
+updateNotifier({ pkg: cli.pkg }).notify();
+
+if (!cli.input[0]) {
+    console.error('Specify a URL');
+    process.exit(1);
+}
+
+ookk(cli.input[0], cli.flags).then(() => {
+    process.exit();
+}).catch(error => {
+    if (error.noStack) {
+        console.error(chalk.red(error.message));
+        process.exit(1);
+    } else {
+        throw error;
+    }
+});
---
+++ readme.md
@@ -0,0 +1,28 @@
+# ookk
+
+> simple like psi repo, but focus in what i need. and use my proxy request.
+
+
+## Use
+
+```
+
+  Usage
+    $ ookk <url>
+
+  Options
+    --key        Google API Key.
+    --strategy   Strategy to use when analyzing the page: mobile|desktop
+
+  Example
+    $ ookk https://zwkang.com --strategy=mobile
+
+```
+
+## Contact me
+
+[ZWkang](https://zwkang.com)
+
+## LICENSE
+
+MIT
